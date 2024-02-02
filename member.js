942f07004149a023e94084292c922ecebced823b
Matthew Cerza <47721459+Matthew92007@users.noreply.github.com>
2024-02-02 12:52:56 -0700

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+  return {
+    name: "John",
+    age: 30,
+    skills: ["JavaScript", "React", "Node.js"],
+  };
+} 
+function helloWorld() {
+  return "Hello World";
+}
