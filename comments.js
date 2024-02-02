e79c81fa0b936bf6865785cef47602ec38b08a96
Matthew Cerza <47721459+Matthew92007@users.noreply.github.com>
2024-02-02 12:57:04 -0700

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,82 @@
+// Create web server
+// Start server
+// Create a route
+// Send the route to the web server
+// Start the server
+// Make request to the server
+// Get the response
+// Send the response to the client
+// Get the response in the client
+
+// Path: comments.js
+// Create web server
+// Start server
+// Create a route
+// Send the route to the web server
+// Start the server
+// Make request to the server
+// Get the response
+// Send the response to the client
+// Get the response in the client
+// Create a route
+// Send the route to the web server
+// Start the server
+// Make request to the server
+// Get the response
+// Send the response to the client
+// Get the response in the client
+
+// Path: comments.js
+// Create web server
+// Start server
+// Create a route
+// Send the route to the web server
+// Start the server
+// Make request to the server
+// Get the response
+// Send the response to the client
+// Get the response in the client
+// Create a route
+// Send the route to the web server
+// Start the server
+// Make request to the server
+// Get the response
+// Send the response to the client
+// Get the response in the client
+// Create a route
+// Send the route to the web server
+// Start the server
+// Make request to the server
+// Get the response
+// Send the response to the client
+// Get the response in the client
+
+// Path: comments.js
+// Create web server
+// Start server
+// Create a route
+// Send the route to the web server
+// Start the server
+// Make request to the server
+// Get the response
+// Send the response to the client
+// Get the response in the client
+// Create a route
+// Send the route to the web server
+// Start the server
+// Make request to the server
+// Get the response
+// Send the response to the client
+// Get the response in the client
+// Create a route
+// Send the route to the web server
+// Start the server
+// Make request to the server
+// Get the response
+// Send the response to the client
+// Get the response in the client
+// Create a route
+// Send the route to the web server
+// Start the server
+// Make request to the server
+//
